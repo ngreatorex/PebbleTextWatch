08f82d9fae6213b316afe1a8e16117d33f5396a9
Timothy Stough <stough@ginkoleaf.net>
2013-05-09 23:57:20 -0700

    fixed a 1 pixel error and regenerated faces.  nested 2 letter date into the inside just, it is required get proper rendering.

--- src/config.h
+++ src/config.h
@@ -2,7 +2,6 @@
 // Watch Face Variants
 #define TimeRenderOh         true
 #define DateSeparatorLine    true
-#define DateTruncTo2Char     false
 #define DateOutsideJustified true
 #define DateFormatUS         true
 ////////////////////////////////////
@@ -19,14 +18,16 @@
 
 #if DateOutsideJustified
 // Outside -> Weekday is left justified and Date is right justified
+#define DateTruncTo2Char false
 #define WeekdayHStart    0
 #define WeekdayHStop     72
-#define DateHStart       65
-#define DateHStop        144
+#define DateHStart       64
+#define DateHStop        143
 #define WeekdayRightJust false
 #define DateRightJust    true
 #else // Inside Justified
 // Inside -> Weekday is right justified and Date is left justified
+#define DateTruncTo2Char true
 #define WeekdayHStart    0
 #define WeekdayHStop     61
 #define DateHStart       65
